5703c4237d9ae916300333489c3a82f03740460e
Robert Nusser <robert.nusser@qvik.com>
2024-01-20 10:10:42 +0200

    the beginning

---
+++ shooter/scenes/player/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+func _process(_delta):
+	# input 
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * 500
+	move_and_slide()
+
+	# lasser shooting
+	if Input.is_action_pressed("primary action"):
+		print("shoot")
+	if Input.is_action_pressed("secondary action"):
+		print("grenade")
+		
+		
